653d305af8913d3950d446c3b2810e3e2619cac9
jcbarcelosrj <jcbarcelosrj@gmail.com>
2023-12-20 21:34:47 -0300

    feat: add in appmodule repository and service project

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -4,14 +4,22 @@ import 'package:job_timercs/app/modules/home/home_module.dart';
 import 'package:job_timercs/app/modules/login/login_module.dart';
 import 'package:job_timercs/app/modules/project/project_module.dart';
 import 'package:job_timercs/app/modules/splash/splash_page.dart';
+import 'package:job_timercs/app/repositories/projects/project_repository.dart';
+import 'package:job_timercs/app/repositories/projects/project_repository_impl.dart';
 import 'package:job_timercs/app/services/auth/auth_service.dart';
 import 'package:job_timercs/app/services/auth/auth_service_impl.dart';
+import 'package:job_timercs/app/services/project/project_service.dart';
+import 'package:job_timercs/app/services/project/project_service_impl.dart';
 
 class AppModule extends Module {
   @override
   List<Bind> get binds => [
         Bind.lazySingleton<AuthService>((i) => AuthServiceImpl()),
         Bind.lazySingleton((i) => DatabaseImpl()),
+        Bind.lazySingleton<ProjectRepository>(
+            (i) => ProjectRepositoryImpl(database: i())),
+        Bind.lazySingleton<ProjectService>(
+            (i) => ProjectServiceImpl(projectRepository: i())),
       ];
 
   @override
